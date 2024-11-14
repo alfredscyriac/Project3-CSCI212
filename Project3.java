0ed5adfd5e78f0c5e17c50c7b233b0c946496f75
alfredscyriac <alfredscyriac@gmail.com>
2024-11-14 18:04:24 -0500

    Remove scanner from Project3.java as it will be in ApplianceGUI.java

--- Project3.java
+++ Project3.java
@@ -3,27 +3,6 @@ public class Project3 {
         SortedApplianceList refrigerators = new SortedApplianceList();
         SortedApplianceList dishwashers = new SortedApplianceList();
         SortedApplianceList microwaves = new SortedApplianceList();
-        
-        TextFileInput fileInput = new TextFileInput("p2input.txt");
-        String line;
-        while ((line = fileInput.readLine()) != null) {
-            String[] parts = line.split(",");
-            String serial = parts[0];
-            double price = Double.parseDouble(parts[1]);
-            if(parts[0].charAt(0) == 'R'){
-                int cubic_feet = Integer.parseInt(parts[2]);
-                refrigerators.add(new Refrigerator(serial, price, cubic_feet));
-            }
-            else if(parts[0].charAt(0)=='M'){
-                int watts = Integer.parseInt(parts[2]);
-                microwaves.add(new Microwave(serial, price, watts));
-            }
-            else if(parts[0].charAt(0)=='D'){
-                boolean undercounterInstallation = parts[2].equalsIgnoreCase("Y");
-                dishwashers.add(new Dishwasher(serial, price, undercounterInstallation));
-            }
-        }
-        fileInput.close();
 
         new ApplianceGUI(refrigerators, dishwashers, microwaves);
     }
