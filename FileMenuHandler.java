d56058328084ee294c966ff71a14214c7e267172
alfredscyriac <alfredscyriac@gmail.com>
2024-11-21 11:29:23 -0500

    Create constructor for FileMenuHandler

--- FileMenuHandler.java
+++ FileMenuHandler.java
@@ -4,5 +4,22 @@ import java.awt.event.ActionListener;
 import java.io.File;
 
 public class FileMenuHandler implements ActionListener{
+    private JFrame frame;
+    private SortedApplianceList refrigerators;
+    private SortedApplianceList dishwashers;
+    private SortedApplianceList microwaves;
+    private JTextArea fridgeArea;
+    private JTextArea dishWasherArea;
+    private JTextArea microwaveArea;
+    public FileMenuHandler(JFrame frame, SortedApplianceList refrigerators, SortedApplianceList dishwashers, SortedApplianceList microwaves,
+                           JTextArea fridgeArea, JTextArea dishWasherArea, JTextArea microwaveArea) {
+        this.frame = frame;
+        this.refrigerators = refrigerators;
+        this.dishwashers = dishwashers;
+        this.microwaves = microwaves;
+        this.fridgeArea = fridgeArea;
+        this.dishWasherArea = dishWasherArea;
+        this.microwaveArea = microwaveArea;
+    }
     
 }
