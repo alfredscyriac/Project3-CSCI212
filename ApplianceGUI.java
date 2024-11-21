809d5629730e8890e1a6c9c9236ab90be7ac9820
alfredscyriac <alfredscyriac@gmail.com>
2024-11-21 13:48:59 -0500

    Remove the outputting from happening in GUI as it happens in File
    clear

--- ApplianceGUI.java
+++ ApplianceGUI.java
@@ -8,38 +8,27 @@ public class ApplianceGUI {
     private JTextArea fridgeArea;
     private JTextArea dishWasherArea;
     private JTextArea microwaveArea;
-        public ApplianceGUI(SortedApplianceList refrigerators, SortedApplianceList dishwashers, SortedApplianceList microwaves){
+    public ApplianceGUI(SortedApplianceList refrigerators, SortedApplianceList dishwashers, SortedApplianceList microwaves){
+        this.refrigerators = refrigerators;
+        this.dishwashers = dishwashers;
+        this.microwaves = microwaves;
         JFrame frame = new JFrame(); 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
-        frame.setLayout(new GridLayout(1, 3)); 
+        frame.setLayout(new BorderLayout());
         frame.setTitle("Project 3 - Appliance List w Modified GUI and Exception for Illegal Appliances"); 
 
-        JMenuBar menuBar = new JMenuBar();
-        JMenu fileMenu = new JMenu("File"); 
+        createMenu(frame);
         
-        JTextArea fridgeArea = new JTextArea();
-        ApplianceNode current = refrigerators.head.next;
-        while (current != null){
-            fridgeArea.append(current.data.toString() + "\n");
-            current = current.next;
-        }
-        frame.add(new JScrollPane(fridgeArea));
+        fridgeArea = new JTextArea();
+        dishWasherArea = new JTextArea();
+        microwaveArea = new JTextArea();
 
-        JTextArea dishWasherArea = new JTextArea();
-        ApplianceNode current2 = dishwashers.head.next; 
-        while(current2 != null){
-            dishWasherArea.append(current2.data.toString()+"\n");
-            current2 = current2.next; 
-        }
-        frame.add(new JScrollPane(dishWasherArea));
+        JPanel appliancePanel = new JPanel(new GridLayout(1, 3));
+        appliancePanel.add(new JScrollPane(fridgeArea));
+        appliancePanel.add(new JScrollPane(dishWasherArea));
+        appliancePanel.add(new JScrollPane(microwaveArea));
 
-        JTextArea microwaveArea = new JTextArea();
-        ApplianceNode current3 = microwaves.head.next;
-        while(current3 != null){
-            microwaveArea.append(current3.data.toString() + "\n");
-            current3=current3.next;
-        }
-        frame.add(new JScrollPane(microwaveArea));
+        frame.add(appliancePanel, BorderLayout.CENTER);
 
         frame.setSize(900, 400);
         frame.setVisible(true);
