e32b6e3891c1dd0c04533ba9efd805f3d6443106
alfredscyriac <alfredscyriac@gmail.com>
2024-11-24 21:19:48 -0500

    Add javadoc to ApplianceNode.java

--- ApplianceNode.java
+++ ApplianceNode.java
@@ -1,7 +1,16 @@
+/**
+ * The ApplianceNode class represents a node in a linked list (ApplianceList.java or SortedApplianceList.java)
+ * Each node holds an Appliance object as its data and a .next which references the next node in the linked list
+ */
 public class ApplianceNode {
     protected Appliance data;
     protected ApplianceNode next;
 
+    /**
+     * This is the constructor for a node 
+     * Initilaizes each nodes .next to be null
+     * @param data, this appliance object is used to store value in the node
+     */
     public ApplianceNode(Appliance data){
         this.data = data;
         this.next=null;
