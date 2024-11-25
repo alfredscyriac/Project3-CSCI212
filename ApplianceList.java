a07ae062a9affb9e6bc244506424be5ad43cb143
alfredscyriac <alfredscyriac@gmail.com>
2024-11-24 21:18:56 -0500

    Add javadoc for ApplianceList.java

--- ApplianceList.java
+++ ApplianceList.java
@@ -1,14 +1,30 @@
+/*
+ * This class is a linked list for appliance
+ * It has instance variables with protected access
+ * .head returns the first element in the linked list 
+ * .last returns the last element in the linked list 
+ * .length returns the length of the linked list
+ */
 public abstract class ApplianceList {
     protected ApplianceNode head;
     protected ApplianceNode last;
     protected int length;
 
+    /*
+     * Constructs an empty linked list with a dummy node as the head 
+     */
     public ApplianceList(){
         this.head = new ApplianceNode(null);
         this.last = head; 
         this.length = 0;
     }
 
+    /*
+     * Adds a node to the end of the linked list 
+     * Increments the length of the linked list by 1
+     * @param appliance needs to be of the Appliance class, this object is used to create a node 
+     * This new node becomes the node we add to the end of the linked list 
+     */
     public void append(Appliance appliance){
         ApplianceNode new_node = new ApplianceNode(appliance);
         last.next = new_node; 
